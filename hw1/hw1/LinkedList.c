4c0b3506f42ba4c92a6fecc53a2b09e8c3fe7907
dizzyfingerz <Grant@Macintosh-150.local>
2014-07-11 03:14:30 -0700

    babyaosidna

--- hw1/hw1/LinkedList.c
+++ hw1/hw1/LinkedList.c
@@ -98,11 +98,13 @@ bool PushLinkedList(LinkedList list, LLPayload_t payload) {
   // STEP 3.
   // typical case; list has >=1 elements
   if (list->num_elements >= 1) {
-    Verify333(list->head != NULL);
-    Verify333(list->tail != NULL);
-    ln->next = list->head;
+    LinkedListNodePtr headnode = list->head;
+
+    Verify333(headnode->prev == NULL);
+
+    ln->next = headnode;
     ln->prev = NULL;
-    list->head->prev = ln;
+    headnode->prev = ln;
     list->num_elements++;
     list->head = ln;
 
